a8fab36c0a20d846a48b0c838147fede0b257479
mouss2388 <mouss2388@gmail.com>
2018-04-02 17:05:24 +0200

    Creation interface graphique + référence des widgets

--- app/src/main/java/com/abdelouahad/mustapha/topquiz/MainActivity.java
+++ app/src/main/java/com/abdelouahad/mustapha/topquiz/MainActivity.java
@@ -1,13 +1,26 @@
 package com.abdelouahad.mustapha.topquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButon;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGreetingText = findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = findViewById(R.id.activity_main_name_input);
+        mPlayButon = findViewById(R.id.activity_main_play_btn);
     }
 }
