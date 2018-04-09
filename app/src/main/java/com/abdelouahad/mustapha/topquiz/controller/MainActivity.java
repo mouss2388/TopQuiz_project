9b0a9585ec561d350419d872c74e3377c4651922
mouss2388 <mouss2388@gmail.com>
2018-04-09 22:57:56 +0200

    Sauvegarde des informations de l'utilisateur (firstname + score) et modification de la phrase d'introduction (func welcomeUser() )

--- app/src/main/java/com/abdelouahad/mustapha/topquiz/controller/MainActivity.java
+++ app/src/main/java/com/abdelouahad/mustapha/topquiz/controller/MainActivity.java
@@ -1,9 +1,11 @@
 package com.abdelouahad.mustapha.topquiz.controller;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.text.Html;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
@@ -21,14 +23,22 @@ public class MainActivity extends AppCompatActivity {
     private Button mPlayButon;
     private User mUser;
     public static final String TAG = MainActivity.class.getSimpleName();
-
     public static final int GAME_ACTIVITY_REQUEST_CODE = 30;
+
+    private SharedPreferences mPreferences;
+
+    public static final String PREF_KEY_SCORE="PREF_KEY_SCORE";
+    public static final String  PREF_KEY_FIRSTNAME="PREF_KEY_FIRSTNAME";
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(GAME_ACTIVITY_REQUEST_CODE==requestCode && RESULT_OK==resultCode){
             int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE,0);
+            //sauvegarde le score de l'utilisateur
+            mPreferences.edit().putInt(PREF_KEY_SCORE,score).apply();
+
             Log.e(TAG,"score:"+score);
+            welcomeUser();
         }
     }
 
@@ -38,13 +48,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Instance d'un nouvel objet utilisateur
         mUser = new User();
-
+        //Preferences accessible seulement depuis cette activité
+        mPreferences =getPreferences(MODE_PRIVATE);
         //Connection aux widgets
         mGreetingText = findViewById(R.id.activity_main_greeting_txt);
         mNameInput = findViewById(R.id.activity_main_name_input);
         mPlayButon = findViewById(R.id.activity_main_play_btn);
         //Désactive le bouton
         mPlayButon.setEnabled(false);
+
+
+        //Check si une partie existe
+        welcomeUser();
 
 
 
@@ -75,6 +90,8 @@ public class MainActivity extends AppCompatActivity {
                 String firstname = mNameInput.getText().toString();
                 //Initialise nom de l'utilisateur
                 mUser.setFirstname(firstname);
+                //sauvegarde le firstname dans les preferences
+                mPreferences.edit().putString(PREF_KEY_FIRSTNAME,mUser.getFirstname()).apply();
                 //Creer un intent pour passer de MainActivity -> GameActivity
                 Intent gameActivityIntent = new Intent(MainActivity.this,GameActivity.class);
                 //Activity avec un identifiant (GAME_ACTIVITY_REQUEST_CODE)
@@ -84,4 +101,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private void welcomeUser(){
+        if(mPreferences.getString(PREF_KEY_FIRSTNAME,null)!=null){
+            //Stock firstname et score dans des variables
+            String firstname=mPreferences.getString(PREF_KEY_FIRSTNAME,null);
+            int score = mPreferences.getInt(PREF_KEY_SCORE,0);
+
+            //Variable contenant la phrase avec des balises HTML pour la mise en forme
+            String salutation= "Welcome back, "+firstname+"!<br/> Your last score was "+score+", will you do better this time?";
+            //Test la version d'ANDROID de l'application
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {//Si SDK >= Version Nougat
+                //Affiche le TextView avec le HTML
+                mGreetingText.setText(Html.fromHtml(salutation,Html.FROM_HTML_MODE_COMPACT));
+            } else { // Si inférieur
+                mGreetingText.setText(Html.fromHtml(salutation));
+            }
+        }
+    }
 }
